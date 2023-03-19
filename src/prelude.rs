b46d24712c1c8e9bf6639605145ac025f00f0218
adamnemecek <adamnemecek@gmail.com>
2023-03-19 00:21:14 -0700

    prelude

---
+++ src/prelude.rs
@@ -0,0 +1,11 @@
+pub use crate::{
+    finite::Finite,
+    group::Group,
+    integers::Integer,
+    mono::{Monomial, MonomialOrder},
+    naturals::Natural,
+    polynomials::Polynomial,
+    rationals::Rational,
+    reals::Real,
+    ring::Ring,
+};
