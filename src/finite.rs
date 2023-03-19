c6d4a0e5eac85e4df6ebc65f558e12b8c686ac1c
adamnemecek <adamnemecek@gmail.com>
2023-03-19 07:55:20 -0700

    finite.0

--- src/finite.rs
+++ src/finite.rs
@@ -5,35 +5,29 @@ use crate::{
     Group, Integer, Natural, Ring,
 };
 
-pub fn finite<const N: Natural>(x: Integer) -> Finite<N> {
-    Finite::from(x)
-}
-
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Finite<const N: Natural> {
-    val: Natural,
-}
+pub struct Finite<const N: Natural>(Natural);
+
 impl<const N: Natural> std::fmt::Debug for Finite<N> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.val.fmt(f)
+        self.0.fmt(f)
     }
 }
+
 impl<const N: Natural> From<Natural> for Finite<N> {
     fn from(i: Natural) -> Self {
         (i % N).into()
     }
 }
+
 impl<const N: Natural> From<Integer> for Finite<N> {
     fn from(i: Integer) -> Self {
         if i >= 0 {
-            Self {
-                val: (i as Natural) % N,
-            }
+            i as Natural
         } else {
-            -Self {
-                val: i.unsigned_abs() % N,
-            }
+            i.unsigned_abs()
         }
+        .into()
     }
 }
 
@@ -41,16 +35,18 @@ impl<const N: Natural> std::ops::Add for Finite<N> {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self {
-        ((self.val + rhs.val) % N).into()
+        (self.0 + rhs.0).into()
     }
 }
+
 impl<const N: Natural> std::ops::Neg for Finite<N> {
     type Output = Self;
 
     fn neg(self) -> Self {
-        ((N - self.val) % N).into()
+        (N - self.0).into()
     }
 }
+
 impl<const N: Natural> std::ops::Sub for Finite<N> {
     type Output = Self;
 
@@ -58,11 +54,13 @@ impl<const N: Natural> std::ops::Sub for Finite<N> {
         self + (-rhs)
     }
 }
+
 impl<const N: Natural> Identity<Addition> for Finite<N> {
     fn identity() -> Self {
         0u128.into()
     }
 }
+
 impl<const N: Natural> Group for Finite<N> {}
 impl<const N: Natural> AbelianGroup for Finite<N> {}
 
@@ -70,21 +68,23 @@ impl<const N: Natural> std::ops::Mul for Finite<N> {
     type Output = Self;
 
     fn mul(self, rhs: Self) -> Self {
-        ((self.val * rhs.val) % N).into()
+        (self.0 * rhs.0).into()
     }
 }
+
 impl<const N: Natural> Identity<Multiplication> for Finite<N> {
     fn identity() -> Self {
         1u128.into()
     }
 }
+
 impl<const N: Natural> Ring for Finite<N> {
     fn multiplicative_inverse(&self) -> Option<Self> {
-        (1..N).find(|x| (x * self.val) % N == 1).map(Self::from)
+        (1..N).find(|x| (x * self.0) % N == 1).map(Self::from)
     }
 
     fn pow(&self, pow: Natural) -> Self {
-        self.val.pow(Natural::from(pow) as u32).into()
+        self.0.pow(Natural::from(pow) as u32).into()
     }
 }
 
@@ -98,4 +98,5 @@ impl<const N: Natural> std::ops::Div for Finite<N> {
             .unwrap_or_else(|| 0u128.into())
     }
 }
+
 impl<const N: Natural> Field for Finite<N> {}
